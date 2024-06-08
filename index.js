33496edfdcb484eef431f9a2fb1b5774b68a7c2d
Vinicius <vinicius.r.o@hotmail.com>
2024-06-08 16:22:16 -0300

    Login arrumado

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname))); // Serve arquivos estáticos no mesmo diretório
 
